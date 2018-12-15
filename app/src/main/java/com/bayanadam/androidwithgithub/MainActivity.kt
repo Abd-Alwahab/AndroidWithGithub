d453f86f41b70dbd130995c7d6ad05487c4bbd08
Abd Alwahab <alwahaba47@gmail.com>
2018-12-15 09:04:52 -0800

    Update Two File and add buttons and text and image to our app

--- app/src/main/java/com/bayanadam/androidwithgithub/MainActivity.kt
+++ app/src/main/java/com/bayanadam/androidwithgithub/MainActivity.kt
@@ -1,14 +1,25 @@
-package com.bayanadam.androidwithgithub
+ package com.bayanadam.androidwithgithub
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Our first Project on Github
+
+
+        val image = findViewById<ImageView>(R.id.imageView)
+        val btn = findViewById<Button>(R.id.button)
+        val text = findViewById<TextView>(R.id.textView)
     }
 }
